c65a1aaa42e39cfe84faf1902fdaa7dd3aa65f78
Alan Somers <asomers@gmail.com>
2019-01-26 18:20:30 -0700

    Don't break downcast on stable while fixing it on nightly
    Issue #1

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,10 @@ fn type_name<T: StdAny + ?Sized>() -> &'static str { unsafe { intrinsics::type_n
 fn type_name<T: StdAny + ?Sized>() -> &'static str { "[ONLY ON NIGHTLY]" }
 
 pub trait Any: StdAny {
+    /// TODO: once 1.33.0 is the minimum supported compiler version, remove
+    /// Any::type_id_compat and use StdAny::type_id instead.
+    /// https://github.com/rust-lang/rust/issues/27745
+    fn type_id_compat(&self) -> TypeId { TypeId::of::<Self>() }
     #[doc(hidden)]
     fn type_name(&self) -> &'static str { type_name::<Self>() }
 }
@@ -115,7 +119,7 @@ fn to_trait_object<T: ?Sized>(obj: &T) -> TraitObject {
 pub trait Downcast<T>: Any
     where T: Any
 {
-    fn is_type(&self) -> bool { self.type_id() == TypeId::of::<T>() }
+    fn is_type(&self) -> bool { self.type_id_compat() == TypeId::of::<T>() }
 
     unsafe fn downcast_ref_unchecked(&self) -> &T { &*(to_trait_object(self).data as *mut T) }
 
